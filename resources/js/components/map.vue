fac29c266e77c295b3125d322cfc181bd96b06e3
willyam.machado <willyam.mach@gmail.com>
2025-06-07 15:26:01 -0300

    feat: add map component with geolocation integration

---
+++ resources/js/components/map.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="w-full h-[60vh] rounded-sm" ref="mapElement">
+
+    </div>
+</template>
+  
+  <script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+  import L from 'leaflet'
+  
+  const mapElement = ref<HTMLElement | null>(null)
+  
+  onMounted(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords
+  
+      const map = L.map(mapElement.value!).setView([latitude, longitude], 16)
+  
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+      }).addTo(map)
+  
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup('Você está aqui!')
+        .openPopup()
+    })
+  })
+</script>
+
+  
