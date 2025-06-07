08d8dcf6a94e4c396069830d98deeae97235053c
willyam.machado <willyam.mach@gmail.com>
2025-06-07 15:26:15 -0300

    feat: create Home page layout with header, map, and footer components

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen p-5 gap-5">
+    <Header></Header>
+    <Map></Map>
+    <Footer class="mt-auto fixed bottom-0 left-0 w-full" />
+  </div>
+</template>
+
+<script setup>
+  import Header from '@/components/header.vue';
+  import Map from '@/components/map.vue';
+  import Footer from '@/components/footer.vue';
+</script>
