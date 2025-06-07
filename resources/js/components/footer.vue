b7d754d385b6a740f9b86393011e3c232577f05b
willyam.machado <willyam.mach@gmail.com>
2025-06-07 15:25:24 -0300

    feat: add footer component with navigation buttons

---
+++ resources/js/components/footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-orange-600 rounded-t-md h-18 flex items-center px-4">
+        <div class="flex gap-4 overflow-x-auto w-full">
+            <button class="text-white whitespace-nowrap px-3 py-2 hover:bg-orange-500 rounded">Item 1</button>
+            <button class="text-white whitespace-nowrap px-3 py-2 hover:bg-orange-500 rounded">Item 2</button>
+        </div>
+    </div>
+</template>
+  
