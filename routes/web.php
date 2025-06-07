aec4d6e9311c610ae827bd194a68ff433db94592
willyam.machado <willyam.mach@gmail.com>
2025-06-07 15:26:35 -0300

    feat: add settings routes for profile and password management

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    return Inertia::render('Home');
+})->name('Home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/home', function () {
+    return Inertia::render('Home');
+})->name('Home');
 
 require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
