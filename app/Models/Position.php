6f9d6e5eff5124e897cbfe4e2333a6624de72ff1
luismunhoz55 <munhozluisfernando@gmail.com>
2025-06-08 14:30:11 -0300

    FEAT: create migrations, models and controllers for Activities and Positions

---
+++ app/Models/Position.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Position extends Model
+{
+    protected $fillable = [
+        'latitude',
+        'longitude',
+        'activity_id'
+    ];
+
+    public function activity()
+    {
+        return $this->belongsTo(Activity::class);
+    }
+}
