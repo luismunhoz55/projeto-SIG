5aa1e317c84714c9e845bf6610bc101485ca33a2
willyam.machado <willyam.mach@gmail.com>
2025-06-14 22:43:42 -0300

    feat: pass positions to map component and refactor tracking logic in Home.vue

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex min-h-screen flex-col gap-5 p-5">
-        <Map />
+        <Map :positions="positions" />
 
         <div class="flex items-center justify-center gap-4">
             <Button v-if="!watchId" @click="startTracking" size="icon"><Play /></Button>
@@ -17,76 +17,76 @@
 </template>
 
 <script setup lang="ts">
-import Map from '@/components/map.vue';
-import { Button } from '@/components/ui/button';
-import { Position } from '@/types/position';
-import { Link, router } from '@inertiajs/vue3';
-import axios from 'axios';
-import { Pause, Play, Square } from 'lucide-vue-next';
-import { ref } from 'vue';
-import { toast, Toaster } from 'vue-sonner';
-import 'vue-sonner/style.css';
+    import Map from '@/components/map.vue';
+    import { Button } from '@/components/ui/button';
+    import { Position } from '@/types/position';
+    import { Link, router } from '@inertiajs/vue3';
+    import axios from 'axios';
+    import { Pause, Play, Square } from 'lucide-vue-next';
+    import { ref } from 'vue';
+    import { toast, Toaster } from 'vue-sonner';
+    import 'vue-sonner/style.css';
 
-const watchId = ref<number>();
-const activityId = ref('');
-const positions = ref<Position[]>([]);
+    const watchId = ref<number>();
+    const activityId = ref('');
+    const positions = ref<Position[]>([]);
 
-const startTracking = async () => {
-    if (!navigator.geolocation) {
-        alert('Geolocalização não suportada.');
-        return;
-    }
+    const startTracking = async () => {
+        if (!navigator.geolocation) {
+            alert('Geolocalização não suportada.');
+            return;
+        }
 
-    toast.info('Sessão iniciada');
+        toast.info('Sessão iniciada');
 
-    const api = axios.create({
-        baseURL: 'http://127.0.0.1:8000',
-    });
+        const api = axios.create({
+            baseURL: 'http://127.0.0.1:8000',
+        });
 
-    const response = await api.post('/activity');
+        const response = await api.post('/activity');
 
-    console.log(response.data.id);
+        console.log(response.data.id);
 
-    activityId.value = response.data.id;
+        activityId.value = response.data.id;
 
-    watchId.value = navigator.geolocation.watchPosition(
-        (pos) => {
-            const { latitude, longitude } = pos.coords;
-            const registered_at = new Date().toISOString();
-            console.log({
-                latitude,
-                longitude,
-                registered_at,
-            });
+        watchId.value = navigator.geolocation.watchPosition(
+            (pos) => {
+                const { latitude, longitude } = pos.coords;
+                const registered_at = new Date().toISOString();
+                console.log({
+                    latitude,
+                    longitude,
+                    registered_at,
+                });
 
-            positions.value.push({ latitude, longitude, registered_at });
-        },
-        (err) => {
-            console.error('Erro ao obter localização:', err);
-        },
-        {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-            timeout: 5000,
-        },
-    );
-};
-
-function stopTracking() {
-    if (watchId.value) {
-        navigator.geolocation.clearWatch(watchId.value);
-        watchId.value = undefined;
-
-        router.patch(
-            route('activity.update', activityId.value),
-            { positions: positions.value },
+                positions.value.push({ latitude, longitude, registered_at });
+            },
+            (err) => {
+                console.error('Erro ao obter localização:', err);
+            },
             {
-                onSuccess: () => toast.success('Sessão encerrada!'),
+                enableHighAccuracy: true,
+                maximumAge: 0,
+                timeout: 5000,
             },
         );
-    }
-}
+    };
 
-// Opcional: encerrar automaticamente ao fechar ou recarregar a aba
-window.addEventListener('beforeunload', stopTracking);
+    function stopTracking() {
+        if (watchId.value) {
+            navigator.geolocation.clearWatch(watchId.value);
+            watchId.value = undefined;
+
+            router.patch(
+                route('activity.update', activityId.value),
+                { positions: positions.value },
+                {
+                    onSuccess: () => toast.success('Sessão encerrada!'),
+                },
+            );
+        }
+    }
+
+    // Opcional: encerrar automaticamente ao fechar ou recarregar a aba
+    window.addEventListener('beforeunload', stopTracking);
 </script>
